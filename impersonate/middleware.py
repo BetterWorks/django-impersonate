395e83b81d83cf603a0f6ed7f1f6ede380a79400
mvantellingen <michaelvantellingen@gmail.com>
2013-05-07 11:05:54 +0200

    Always add the is_impersonate attribute and set the real user as attribute

--- impersonate/middleware.py
+++ impersonate/middleware.py
@@ -3,9 +3,13 @@ from .helpers import check_allow_for_user, check_allow_for_uri
 
 class ImpersonateMiddleware(object):
     def process_request(self, request):
+        request.user.is_impersonate = False
+
         if request.user.is_authenticated() and \
            '_impersonate' in request.session:
             new_user = request.session['_impersonate']
+            new_user.real_user = request.user
+
             if check_allow_for_user(request, new_user) and \
                check_allow_for_uri(request.path):
                 request.user = new_user
