6c13bf5c64663466359da7178b3f8c12b929df56
Peter Sanchez <petersanchez@gmail.com>
2014-03-14 12:30:38 -0700

    Moved LOGIN_URL to function to avoid circular import issues.

--- impersonate/decorators.py
+++ impersonate/decorators.py
@@ -6,21 +6,22 @@ from django.contrib.auth import REDIRECT_FIELD_NAME
 from .helpers import get_redir_path, check_allow_impersonate
 
 
-# support named URL patterns from Django 1.5 onwards
-if django.VERSION >= (1, 5):
-    from django.shortcuts import resolve_url
-    from django.utils.encoding import force_str
+def get_login_url():
+    # support named URL patterns from Django 1.5 onwards
+    if django.VERSION >= (1, 5):
+        from django.shortcuts import resolve_url
+        from django.utils.encoding import force_str
 
-    LOGIN_URL = force_str(resolve_url(settings.LOGIN_URL))
-else:
-    LOGIN_URL = settings.LOGIN_URL
+        return force_str(resolve_url(settings.LOGIN_URL))
+    else:
+        return settings.LOGIN_URL
 
 
 def allowed_user_required(view_func):
     def _checkuser(request, *args, **kwargs):
         if not request.user.is_authenticated():
             return redirect('{0}?{1}={2}'.format(
-                LOGIN_URL,
+                get_login_url(),
                 REDIRECT_FIELD_NAME,
                 urlquote(request.get_full_path()),
             ))
