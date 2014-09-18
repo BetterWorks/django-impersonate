61b4c84d072e9a409c1335cd28bcb9670e222755
Jason Spafford <nullprogrammer@gmail.com>
2014-09-17 17:38:57 -0700

    Removed stop requirement before impersonate
    You can now impersonate users without stopping
    impersonation. Added tests too.

---
+++ impersonate/contexts.py
@@ -0,0 +1,80 @@
+from django.conf import settings
+from django.db.models import Q
+from .helpers import (
+    get_redir_arg, get_paginator, get_redir_field, users_impersonable
+)
+
+try:
+    # Django 1.5 check
+    from django.contrib.auth import get_user_model
+except ImportError:
+    from django.contrib.auth.models import User
+else:
+    User = get_user_model()
+
+
+def get_list_template_context(request):
+    ''' List all users in the system.
+        Will add 5 items to the context.
+          * users - queryset of all users
+          * paginator - Django Paginator instance
+          * page - Current page of objects (from Paginator)
+          * page_number - Current page number, defaults to 1
+          * redirect - arg for redirect target, e.g. "?next=/foo/bar"
+    '''
+    users = users_impersonable(request)
+
+    paginator, page, page_number = get_paginator(request, users)
+
+    return {
+        'users': users,
+        'paginator': paginator,
+        'page': page,
+        'page_number': page_number,
+        'redirect': get_redir_arg(request),
+        'redirect_field': get_redir_field(request),
+    }
+
+
+def get_search_template_context(request, query):
+    ''' Simple search through the users.
+        Will add 7 items to the context.
+          * users - All users that match the query passed.
+          * paginator - Django Paginator instance
+          * page - Current page of objects (from Paginator)
+          * page_number - Current page number, defaults to 1
+          * query - The search query that was entered
+          * redirect - arg for redirect target, e.g. "?next=/foo/bar"
+          * redirect_field - hidden input field with redirect argument,
+                              put this inside search form
+    '''
+
+    # get username field
+    username_field = getattr(User, 'USERNAME_FIELD', 'username')
+
+    # define search fields and lookup type
+    search_fields = set(getattr(settings, 'IMPERSONATE_SEARCH_FIELDS',
+                         [username_field, 'first_name', 'last_name', 'email']))
+    lookup_type = getattr(settings, 'IMPERSONATE_LOOKUP_TYPE', 'icontains')
+
+    # prepare kwargs
+    search_q = Q()
+    for term in query.split():
+        sub_q = Q()
+        for search_field in search_fields:
+            sub_q |= Q(**{'{0}__{1}'.format(search_field, lookup_type): term})
+        search_q &= sub_q
+
+    users = users_impersonable(request)
+    users = users.filter(search_q)
+    paginator, page, page_number = get_paginator(request, users)
+
+    return {
+        'users': users,
+        'paginator': paginator,
+        'page': page,
+        'page_number': page_number,
+        'query': query,
+        'redirect': get_redir_arg(request),
+        'redirect_field': get_redir_field(request),
+    }
---
+++ impersonate/logic.py
@@ -0,0 +1,41 @@
+from .decorators import allowed_user_required
+from .helpers import check_allow_for_user
+from .signals import session_begin, session_end
+
+
+@allowed_user_required
+def impersonate(request, new_user):
+    ''' Takes in the UID of the user to impersonate.
+    View will fetch the User instance and store it
+    in the request.session under the '_impersonate' key.
+
+    The middleware will then pick up on it and adjust the
+    request object as needed.
+    '''
+    if check_allow_for_user(request, new_user):
+        request.session['_impersonate'] = new_user.id
+        request.session.modified = True  # Let's make sure...
+        # can be used to hook up auditing of the session
+        session_begin.send(
+            sender=None,
+            impersonator=request.user,
+            impersonating=new_user,
+            request=request
+        )
+
+
+def stop_impersonate(request):
+    ''' Remove the impersonation object from the session
+    '''
+    impersonating = request.session.pop('_impersonate', None)
+    if impersonating is not None:
+        request.session.modified = True
+        request.user.is_impersonate = False
+        request.user = request.impersonator
+
+        session_end.send(
+            sender=None,
+            impersonator=request.impersonator,
+            impersonating=impersonating,
+            request=request
+        )
